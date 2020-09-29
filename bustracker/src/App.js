eeacf3afc868d8bddd0d4edc3d06faf1901038da
Nguyen Vu <vu.n@sutrixsolutions.com>
2020-09-29 18:11:18 +0700

    add

--- bustracker/src/App.js
+++ bustracker/src/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import Map from './Map'
+import io from 'socket.io-client';
+ import layer from './layers'
+const socket = io('http://localhost:8888');
+const obj = {
+
+}
+socket.on("init", list => {
+  list.forEach(item => {
+    console.log(item)
+    const marker = new window.google.maps.Marker({
+      position: { lat: parseFloat(item.coordinates[1]), lng: parseFloat(item.coordinates[0]) },
+      icon:{
+        url: "http://localhost:3000/bus.png",
+        size:10
+      },
+      
+      map:  window.map,
+    });
+    obj[item.id] = marker
+  })
+})
+
+socket.on('change', listChange => {
+  listChange.forEach(item => {
+    if(obj[item.id]){
+      obj[item.id].setPosition({
+        lat: parseInt(item.coordinates[1]),
+        lng: parseInt(item.coordinates[0])
+      })
+      return
+    }
+    const marker = new window.google.maps.Marker({
+      position: { lat: parseFloat(item.coordinates[1]), lng: parseFloat(item.coordinates[0]) },
+      icon:{
+        url: "/bus.png",
+        size:10
+      },
+      map:  window.map,
+    });
+    obj[item.id] = marker
+  })
+})
 
 function App() {
   return  <Map
   id="myMap"
   options={{
-    center: { lat: 41.0082, lng: 28.9784 },
+    center: { lat: 42.05515, lng: -87.76637 },
     zoom: 8
   }}
   onMapLoad={map => {
+    window.map = map
+    layer(map)
     var marker = new window.google.maps.Marker({
       position: { lat: 41.0082, lng: 28.9784 },
       map: map,
--- bustracker/src/layers/index.js
+++ bustracker/src/layers/index.js
@@ -1,6 +1,26 @@
 import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
 import {GeoJsonLayer} from '@deck.gl/layers';
 
-class CustomLayers {
-    
+function CustomGeoJsonLayer(map){
+    const overlay = new DeckOverlay({
+        layers: [
+          new GeoJsonLayer({
+            data: "http://localhost:8888/routes.json",
+            pickable: true,
+    stroked: false,
+    filled: true,
+    extruded: true,
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [160, 160, 180, 200],
+    getRadius: 100,
+    getLineWidth: 1,
+    getElevation: 30
+          })
+        ]
+      });
+      // Add overlay to map
+      overlay.setMap(map);
 }
+
+export default CustomGeoJsonLayer
